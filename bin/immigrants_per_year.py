b1cc4aa48d5f3640675d45571225001df98ae6ad
Alejandra Camelo Cruz <jaime.camelo.cruz@uni-potsdam.de>
2023-05-23 01:33:33 +0200

    Modifications to the first line-plot function. It is now functional and flags work from terminal. Result plot is saved and displayed in results folder

--- bin/immigrants_per_year.py
+++ bin/immigrants_per_year.py
@@ -12,33 +12,47 @@ import seaborn as sns
 
 def main(args):
     df = pd.read_csv(args.data)
+    df = df.loc[df['country'] != 'Total']
+    
     end_year = 2021-args.years
     selected_years = [year for year in range(2021,end_year,-1)]
     df = df.loc[df['year'].isin(selected_years)]
     
     if args.country:
-        df = df.loc[df['country'] == args.country]
+        df_countries = df.loc[df['country'] == args.country]
     else:
-        df = df.loc[df['country'] != 'Total'].sort_values("foreigners from foreign countries", 
-                                                           ascending=False)
-        df = df.drop_duplicates(subset=["year"], keep="first")
+        most_migrant = df.loc[df['year'] == 2021]\
+            .sort_values('foreigners from foreign countries',
+                         ascending=False).head(args.num)
+        most_migrant = list(most_migrant.iloc[:,1])
+        df_countries = df.loc[df['country'].isin(most_migrant)]
+        
+    line_plot = sns.lineplot(data=df_countries, x='year',\
+                 y='foreigners from foreign countries', hue='country')
+    line_plot.set(xticks=selected_years)
     
+    fig = line_plot.get_figure()
+    fig.savefig(args.outfile)
     
-    sns.lineplot(data=df, x='year', y='foreigners from foreign countries')
 
 if __name__ == '__main__':
-    data_path = pth.get_relative_path('data/12711-0008-Migration between Germany and foreign countries.csv')
+    data_path = pth.get_relative_path(
+        'data/12711-0008-Migration between Germany and foreign countries.csv')
+    output_path = pth.get_relative_path(
+        'results/immigrants_per_year.png')
+    
     parser = argparse.ArgumentParser(description=__doc__)
     
     parser.add_argument('data', type=str,
                         nargs='?', default=data_path)
-    # parser.add_argument('outfile', type=str,
-    #                     default='immigrants_per_year.png', help='optional output file name')
+    parser.add_argument('--outfile', type=str,
+                        default=output_path,
+                        help='optional output file name')
     parser.add_argument('-y', '--years', type=int, default=5,
                         help='number of years to be plotted (max 21)')
-    # parser.add_argument('-n','--num', type=int, default=5,
-    #                     help='number of countries to be plotted\
-    #                         (ordered by the n countries with most migrants)')
+    parser.add_argument('-n','--num', type=int, default=5,
+                        help='number of countries to be plotted\
+                            (ordered by the n countries with most migrants)')
     parser.add_argument('-c','--country', type=str, default=None,
                         help='country to be plotted')
     
